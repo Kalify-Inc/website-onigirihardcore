dcefb49dc47fa8d85808ae5dd7a583072b388d9f
Anderson Marlon <yagasakiwanderlust@proton.me>
2025-12-02 15:13:04 -0300

    Update: Modules + Fix: SnowOverlay - 02-12 15:13

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import '../styles/globals.css'
 import { Toaster } from 'sonner'
 import { Analytics } from '@vercel/analytics/react';
+import SnowOverlay from '../src/components/SnowOverlay';
 
 // eslint-disable-next-line react/prop-types
 function MyApp({ Component, pageProps }) {
@@ -17,6 +18,7 @@ function MyApp({ Component, pageProps }) {
             <Component {...pageProps} />
             <Analytics />
             <Toaster richColors position="top-right" />
+            <SnowOverlay/>
         </>
     )
 }
